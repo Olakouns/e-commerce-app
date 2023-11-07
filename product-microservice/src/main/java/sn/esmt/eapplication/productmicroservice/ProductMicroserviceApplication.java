416fbf56e7644c88a8fd24dff5ec8b829a0c5272
olakouns <kounassolazare@gmail.com>
2023-11-07 16:49:54 +0000

    first commit

---
+++ product-microservice/src/main/java/sn/esmt/eapplication/productmicroservice/ProductMicroserviceApplication.java
@@ -0,0 +1,11 @@
+package sn.esmt.eapplication.productmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductMicroserviceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductMicroserviceApplication.class, args);
+    }
+}
